c4f13341f077d53e53bbaa4b4d25f50972ea19bc
lizardo-andres12 <lizardo.andres12@gmail.com>
2023-09-23 16:46:42 -0400

    Second Commit

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,12 +1,29 @@
 public class DoublyLinkedList {
 
-    public Node next;
-    public Node prev;
+    public Node head;
+    public Node tail;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+    }
 
     class Node {
-        public double num;
+        public double data;
         public char operand;
         public Node next;
         public Node prev;
+
+        public Node(double data) {
+            this.data = data;
+            prev = null;
+            next = null;
+        }
+
+        public Node(char operand) {
+            this.operand = operand;
+            prev = null;
+            next = null;
+        }
     }
 }
