85b9e2b60cb5922b1fe45bab6c2953c232de5d15
lizardo-andres12 <lizardo.andres12@gmail.com>
2023-09-23 17:36:47 -0400

    Second Commit

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -9,21 +9,37 @@ public class DoublyLinkedList {
     }
 
     class Node {
-        public double data;
-        public char operand;
+        public String data;
         public Node next;
         public Node prev;
 
-        public Node(double data) {
+        public Node(String data) {
             this.data = data;
-            prev = null;
             next = null;
+            prev = null;
         }
+    }
 
-        public Node(char operand) {
-            this.operand = operand;
-            prev = null;
-            next = null;
+    public void addNode(String data) { // adding to the end of the list
+        Node node = new Node(data);
+        // if there is not a tail to the list, the list is empty
+        if (tail == null) {
+            head = node; // head must be the same as tail if there's only one node in the list
+            tail = node; // setting the end equal to the new node
         }
+        // if there is a tail, only the tail must be set to the new node
+        else{
+            tail.next = node; // current tail node is set before the new node
+            node.prev = tail; // new node's prev is set to point to the previous tail
+            tail = node; // sets the tail in the class to reference the new node
+        }
+    }
+
+    public void resolveParenthesis() {
+
+    }
+
+    public void resolveExponent() {
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        // test cases
+        doublyLinkedList.addNode("String");
+        doublyLinkedList.addNode("f");
+        doublyLinkedList.addNode("1234");
+
+        DoublyLinkedList.Node currentNode = doublyLinkedList.head;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
     }
 }
