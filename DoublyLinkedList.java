dff878e353709837287cfc241c97441015218bb1
lizardo-andres12 <lizardo.andres12@gmail.com>
2023-09-23 16:31:31 -0400

    First Commit

---
+++ DoublyLinkedList.java
@@ -0,0 +1,12 @@
+public class DoublyLinkedList {
+
+    public Node next;
+    public Node prev;
+
+    class Node {
+        public double num;
+        public char operand;
+        public Node next;
+        public Node prev;
+    }
+}
